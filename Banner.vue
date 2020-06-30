25d8c631d912109556910a6c9e368500f8f5a4af
goodbreathseth <goodbreathseth@gmail.com>
2020-06-30 12:31:54 -0600

    Added phone number

--- Banner.vue
+++ Banner.vue
@@ -5,9 +5,13 @@
         <img src="/images/icons/Edit.svg" alt="Edit" width="25px" />
       </router-link>
     </button>
-    <button class="absolute text-xl right-0 top-0 iconBtn sm:m-1" @click="$emit('close')">
+    <!-- <button class="absolute text-xl right-0 top-0 iconBtn sm:m-1" @click="$emit('close')">
       <font-awesome-icon icon="times" />
-    </button>
+    </button> -->
+    <a href="tel:8014104998" class="absolute right-0 mr-2 sm:mr-0 sm:right-2 top-1 text-white hover:text-gray-300">
+      <font-awesome-icon class="mr-2 text-xl -mb-px" icon="phone-alt" />
+      <span class="hidden sm:inline-block">(801) 410-4998</span>
+    </a>
     <div class="bg-primary py-4 pr-10 pl-3 font-medium text-white" v-html="layout.content"></div>
   </div>
 </template>
