a97113c6d6ebd7b08097dc10412770411992ae35
Gabe <gabrielsuttner@gmail.com>
2020-08-25 13:08:00 -0600

    added home button to banner

--- Banner.vue
+++ Banner.vue
@@ -5,6 +5,19 @@
         <img src="/images/icons/Edit.svg" alt="Edit" width="25px" />
       </router-link>
     </button>
+    <router-link
+      class="absolute left-0 ml-2 sm:ml-0 sm:left-2 top-1 text-white hover:text-neutral"
+      to="/"
+    >
+      <font-awesome-icon class="mr-2 text-xl -mb-px" icon="home" /><span
+        class="hidden sm:inline-block"
+      >
+        Home
+      </span>
+    </router-link>
+
+    <div class="bg-primary py-4 px-10 font-medium text-white" v-html="layout.content"></div>
+
     <a
       href="tel:8014104998"
       class="absolute right-0 mr-2 sm:mr-0 sm:right-2 top-1 text-white hover:text-neutral"
@@ -12,7 +25,6 @@
       <font-awesome-icon class="mr-2 text-xl -mb-px" icon="phone-alt" />
       <span class="hidden sm:inline-block">(801) 410-4998</span>
     </a>
-    <div class="bg-primary py-4 pr-10 pl-3 font-medium text-white" v-html="layout.content"></div>
   </div>
 </template>
 
