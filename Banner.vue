e51149b8162a589226fd9c7c175c487c6fc6042a
Gabe <gabrielsuttner@gmail.com>
2020-11-02 22:14:11 -0700

    update

--- Banner.vue
+++ Banner.vue
@@ -16,10 +16,10 @@
       </span>
     </router-link>
 
-    <!-- <div class="bg-primary py-4 px-10 font-medium text-white" v-html="layout.content"></div> -->
-    <div class="bg-feedbackError py-4 px-10 font-medium text-white">
+    <div class="bg-primary py-4 px-10 font-medium text-white" v-html="layout.content"></div>
+    <!-- <div class="bg-feedbackError py-4 px-10 font-medium text-white">
       This site is currently under construction. No pricing is final.
-    </div>
+    </div> -->
 
     <a
       href="tel:8014104998"
